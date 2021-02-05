a28fa21899d9733044cc2da5c38dead07c181b84
Victor <vmlc1499@gmail.com>
2021-02-05 03:59:27 -0600

    Code added into 'Command' and 'CommandWords'

--- ZuulGame/src/com/game/Command.java
+++ ZuulGame/src/com/game/Command.java
@@ -1,4 +1,29 @@
 package com.game;
 
 public class Command {
+
+    public Command(String action, String direction) {
+        commandWord = action;
+        this.direction = direction;
+    }
+
+    public String get_CommandWord() {
+        return commandWord;
+    }
+
+    public String get_Direction() {
+        return direction;
+    }
+
+    public boolean is_Unknown() {
+        return (commandWord == null);
+    }
+
+    public boolean has_Direction() {
+        return (direction != null);
+    }
+
+    private String commandWord;
+    private String direction;
+
 }
--- ZuulGame/src/com/game/CommandWords.java
+++ ZuulGame/src/com/game/CommandWords.java
@@ -1,4 +1,16 @@
 package com.game;
 
 public class CommandWords {
+
+    private static final String[] commands = { "go", "quit", "help" };
+
+    public boolean is_Command(String aString) {
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i].equals(aString)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
 }
