4eb7058cd76a546c99e1d9e0e1c2b93287c18f9b
Mario <a18016317@laumnos.uady.mx>
2021-02-06 01:30:42 -0600

    Comment added into 'CommandWords' class

--- ZuulGame/src/com/game/CommandWords.java
+++ ZuulGame/src/com/game/CommandWords.java
@@ -6,6 +6,11 @@ public class CommandWords {
             "go", "quit", "help"
     };
 
+    /**
+     * Check if the input of the user is a valid command. 
+     * @return true if a given string is a valid command,
+     *         false otherwise
+     */
     public boolean is_Command(String aString) {
         for (String command : commands) {
             if (command.equals(aString)) {
