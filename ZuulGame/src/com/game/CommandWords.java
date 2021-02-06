c5c380a3b2329fbf8ca2e3a4efccf5f9c528612a
Eberth <a18016312@alumnos.uady.mx>
2021-02-06 00:23:14 -0600

    Correct the name of the function that verifys if the word input is a command

--- ZuulGame/src/com/game/CommandWords.java
+++ ZuulGame/src/com/game/CommandWords.java
@@ -6,7 +6,7 @@ public class CommandWords {
             "go", "quit", "help"
     };
 
-    public boolean is_Command(String aString) {
+    public boolean isCommand(String aString) {
         for (String command : commands) {
             if (command.equals(aString)) {
                 return true;
