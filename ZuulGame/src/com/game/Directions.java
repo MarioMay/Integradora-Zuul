676b4c41ab41cf8ce4d6bd503145f0c7cb3cfb78
Victor <vmlc1499@gmail.com>
2021-02-07 00:06:39 -0600

    Added 'Directions' class to apply the refactoring 'Replace Type Code with Class',  not finished yet

---
+++ ZuulGame/src/com/game/Directions.java
@@ -0,0 +1,10 @@
+package com.game;
+
+public class Directions {
+
+    private static final String NORTH = "north";
+    private static final String SOUTH = "south";
+    private static final String EAST = "east";
+    private static final String WEST = "west";
+
+}
