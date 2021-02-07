42f60afafa56f7670795aea26679e7e0b2b875ca
Victor <vmlc1499@gmail.com>
2021-02-07 00:38:12 -0600

    Replace Type Code with Class 1st part done

--- ZuulGame/src/com/game/Directions.java
+++ ZuulGame/src/com/game/Directions.java
@@ -7,4 +7,27 @@ public class Directions {
     private static final String EAST = "east";
     private static final String WEST = "west";
 
+    /**
+     * Returns the directions available to navigate of a specific room
+     * @param room Receives a object room (outside, lab, etc...)
+     * @return directions  
+     */
+    public String getExitDirections(Room room) {
+        String directions = " ";
+
+        if (room.getExitRoomByKey(NORTH) != null) {
+            directions += NORTH + " ";
+        }
+        if (room.getExitRoomByKey(EAST) != null) {
+            directions += EAST + " ";
+        }
+        if (room.getExitRoomByKey(SOUTH) != null) {
+            directions += SOUTH + " ";
+        }
+        if (room.getExitRoomByKey(WEST) != null) {
+            directions += WEST + " ";
+        }
+
+        return directions;
+    }
 }
